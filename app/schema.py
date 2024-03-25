c1d52ed03fbbb76fdd982fcfa3573f91c8ca3106
A-Quaglia <augustoquaglia@outlook.com>
2024-03-25 00:20:33 -0300

    feat: ETL trasformar

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,7 @@
 import pandera as pa
 from pandera.typing import DataFrame, Series
 
+email_regex = r"[^@]+@[^@]+\.[^@]+"
 
 class ProdutoSchema(pa.SchemaModel):
     """
@@ -12,13 +13,21 @@ class ProdutoSchema(pa.SchemaModel):
         quantidade (pandas.Series[int]): Série contendo quantidades de produtos (deve ser maior ou igual a 0).
         preco (pandas.Series[float]): Série contendo preços de produtos (deve ser maior ou igual a 0).
         categoria (pandas.Series[str]): Série contendo categorias de produtos.
+        email (Series[str]): E-mail associado ao produto, deve seguir o formato padrão de e-mails.
     """
     id_produto: Series[int]
     nome: Series[str]
     quantidade: Series[int] = pa.Field(ge=0)
     preco: Series[float] = pa.Field(ge=0)
     categoria: Series[str]
+    email: Series[str] = pa.Field(regex=email_regex)
 
     class Config:
         coerce = True
         # strict = True
+
+
+class ProdutoSchemaKPI(ProdutoSchema):
+    valor_total_estoque: Series[float] = pa.Field(ge=0)
+    categoria_normalizada: Series[str]  # Assume-se que a categoria será uma string, não precisa de check específico além de ser uma string
+    disponibilidade: Series[bool]  # Disponibilidade é um booleano, então não precisa de check específico
